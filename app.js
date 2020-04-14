3ee8699d6f1b14174570238fca4ee0d402184717
Amy Oulton <amyeoulton@gmail.com>
2020-04-14 09:28:39 -0700

    created app.js and initialized all required assets, created server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+
+app.use(cookieParser());
+
+const PORT = 3500;
+const ADDRESS = 'localhost'; // 127.0.0.1
+app.listen(PORT, ADDRESS, () => {
+  console.log(`Server listening on http://${ADDRESS}:${PORT}`);
+});
