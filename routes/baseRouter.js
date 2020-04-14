926c4ad3ffd8aae1fc051cf6776a46b007c41610
Amy Oulton <amyeoulton@gmail.com>
2020-04-14 15:17:20 -0700

    added index page, update navigation to return index instead of signin where applicable

--- routes/baseRouter.js
+++ routes/baseRouter.js
@@ -4,21 +4,27 @@ const router = express.Router();
 
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7; // A week in milliseconds
 
+//greets new user with the home page
+
 router.get('/', (request, response) => {
-  response.render('signIn');
+  response.redirect('clucks/index');
 });
+
+//allows sign in function to happen, redirects the user to the index page
 
 router.post('/sign_in', (request, response) => {
   const params = request.body;
   console.log('request.body: ', params);
 
   response.cookie('username', params.username, { maxAge: COOKIE_MAX_AGE });
-  response.redirect('/');
+  response.redirect('/clucks/index');
 });
+
+//allows the sign out page to happen, redirects user to the index page
 
 router.post('/sign_out', (request, response) => {
   response.clearCookie('username');
-  response.redirect('/');
+  response.redirect('/clucks/index');
 });
 
 module.exports = router;
--- routes/clucksRouter.js
+++ routes/clucksRouter.js
@@ -2,13 +2,18 @@ const express = require('express');
 const knex = require('../db/connections');
 const router = express.Router();
 
-router.get('/', (request, response) => {
+// when click on the cluck! button to create a new cluck, it brings the user to the new page if signed in
+//if not signed in, it redirects them to the sign in page
+
+router.get('/new', (request, response) => {
   const username = request.cookies.username;
   if (username) {
     response.render('clucks/new');
   } else response.render('SignIn');
 });
 
+// after the user fills out the form on the new page, data is sent to the clucks table in the db
+// they are then redirected to the index page
 router.post('/', (request, response) => {
   const username = request.cookies.username;
 
@@ -21,12 +26,16 @@ router.post('/', (request, response) => {
         image_url: cluckInfo.image_url
       })
       .returning('*')
-      .then(post => {
-        response.redirect(`/`);
-      });
-  } else {
-    response.redirect('/');
+      .then(response.redirect(`clucks/index`));
   }
+});
+
+router.get('/index', (request, response) => {
+  knex('clucks')
+    .orderBy('created_at', 'desc')
+    .then(clucks => {
+      response.render('clucks/index', { clucks });
+    });
 });
 
 module.exports = router;
