3bd1a93b8d1ff83331599a8ca470ac0a08aab9db
Amy Oulton <amyeoulton@gmail.com>
2020-04-14 10:05:17 -0700

    added nav bar foundation, added chicken icon

--- routes/baseRouter.js
+++ routes/baseRouter.js
@@ -2,4 +2,8 @@ const express = require('express');
 // const knex = require('../db/connections');
 const router = express.Router();
 
+router.get('/', (request, response) => {
+  response.render('signIn');
+});
+
 module.exports = router;
