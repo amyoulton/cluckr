54a27b9188f9066a6a29120db8a155ff0a164065
Amy Oulton <amyeoulton@gmail.com>
2020-04-14 14:05:30 -0700

    added cluck creation page, added sign in stipulation

--- routes/clucksRouter.js
+++ routes/clucksRouter.js
@@ -2,4 +2,31 @@ const express = require('express');
 const knex = require('../db/connections');
 const router = express.Router();
 
+router.get('/', (request, response) => {
+  const username = request.cookies.username;
+  if (username) {
+    response.render('clucks/new');
+  } else response.render('SignIn');
+});
+
+router.post('/', (request, response) => {
+  const username = request.cookies.username;
+
+  if (username) {
+    const cluckInfo = request.body;
+    knex('clucks')
+      .insert({
+        username: username,
+        content: cluckInfo.content,
+        image_url: cluckInfo.image_url
+      })
+      .returning('*')
+      .then(post => {
+        response.redirect(`/`);
+      });
+  } else {
+    response.redirect('/');
+  }
+});
+
 module.exports = router;
